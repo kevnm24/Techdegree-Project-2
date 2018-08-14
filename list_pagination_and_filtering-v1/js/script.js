50477b3cef26ecaaa561fa782941151f0f831770
Kevin Mendez <kevnm24@gmail.com>
2018-08-14 15:14:10 -0700

    i have hit a bump

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -5,6 +5,10 @@ FSJS project 2 - List Filter and Pagination
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const studentList = document.querySelectorAll('.student-item');
+const mainDiv = document.getElementsByTagName('div')[0];
+const pageSize = 10;
+const countStudents = studentList.length;
+const pageCount = Math.ceil(countStudents / pageSize);
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
@@ -16,24 +20,29 @@ function hideStudents() {
 hideStudents();
 
 function showStudents() {
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < pageSize * pageCount; i += 1) {
     studentList[i].style.display = 'block';
   }
 }
 // Create and append the pagination links - Creating a function that can do this is a good approach
 function button() {
-const pageSize = 10;
-const countStudents = studentList.length;
-const pageCount = Math.ceil(countStudents / pageSize);
 for (let i = 1; i <= pageCount; i += 1) {
-  const pagination = document.createElement('pagination');
-  pagination.innerHTML = '<ul><li><a class="active" href ="#">' + i + '</a></li></ul>';
-  const div = document.getElementsByTagName('div')[0];
-  div.appendChild(pagination);
-}
-}
-showStudents();
-button();
 
+  const pagination = document.createElement('div');
+  pagination.classList.add('pagination');
+  mainDiv.appendChild(pagination);
+  const ulList = document.createElement('ul');
+  mainDiv.appendChild(pagination);
+  pagination.appendChild(ulList);
+  ulList.innerHTML = '<li><a class="active" href ="#">' + i + '</a></li>';
+  showStudents();
+}
+}
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
+
+ulList.addEventListener('click', (e) => {
+  if (e.target.tagName === 'pagination'){
+    button();
+  }
+});
